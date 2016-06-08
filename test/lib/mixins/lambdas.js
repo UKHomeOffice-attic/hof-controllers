e2c1f1e0ceba20e49835a6c05b0465668f260a44
joefitter <hello@joefitter.com>
2016-06-08 12:22:19 +0100

    bugfix - res.locals weren't being passed to scope in renderField lambda - this resulted in the value not being persisted when a completed field is revisited

--- test/lib/mixins/lambdas.js
+++ test/lib/mixins/lambdas.js
@@ -18,10 +18,19 @@ describe('Lambdas Mixins', () => {
   });
 
   describe('renderField', () => {
+    const scope = {
+      key: 'a-key',
+      mixin: 'a-mixin'
+    };
     let renderField;
+    let mixinStub;
+    let renderFieldMixin;
 
     beforeEach(() => {
       renderField = res.locals.renderField;
+      mixinStub = sinon.stub();
+      renderFieldMixin = renderField();
+      res.locals['a-mixin'] = () => mixinStub;
     });
 
     it('should return a function', () => {
@@ -29,15 +38,15 @@ describe('Lambdas Mixins', () => {
     });
 
     it('should lookup a mixin from res.locals and call it with key if found', () => {
-      const mixinStub = sinon.stub();
-      const renderFieldMixin = renderField();
-      const scope = {
-        key: 'a-key',
-        mixin: 'a-mixin'
-      };
-      res.locals['a-mixin'] = () => mixinStub;
       renderFieldMixin.call(scope);
       mixinStub.should.have.been.calledOnce.and.calledWithExactly('a-key');
+    });
+
+    it('exposes res.locals to the called mixin', () => {
+      mixinStub.returnsThis();
+      res.locals.foo = 'bar';
+      renderFieldMixin.call(scope);
+      mixinStub.returnValues[0].should.have.property('foo').and.equal('bar');
     });
   });
 });
